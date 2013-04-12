22151d585f04a40f65cd00cd61762b184840b13d
Walter Huf <hufman@gmail.com>
2013-04-12 12:40:12 -0500

    Decode invalid utf-8 loosely, before parsing the HTML

--- vgmdb/main.py
+++ vgmdb/main.py
@@ -10,6 +10,7 @@ def hello():
 @route('/artist/<artist:int>')
 def artist(artist):
 	data = urllib.urlopen('http://vgmdb.net/artist/%s'%artist).read()
+	data = data.decode('utf-8', 'ignore')	# some pages have broken utf8
 	artist_info = vgmdb.artist.parse_artist_page(data)
 	response.content_type = 'application/json; charset=utf-8'
 	return json.dumps(artist_info, sort_keys=True, indent=4, separators=(',',': '), ensure_ascii=False)
