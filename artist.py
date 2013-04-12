ebaefdd7c10053ecec21622701ba4dd3b4ba5b05
Walter Huf <hufman@gmail.com>
2013-04-12 12:41:20 -0500

    Commandline test program to show artist info

---
+++ artist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import urllib
+import vgmdb.artist
+import json
+
+def parse_artist(artist):
+	data = urllib.urlopen('http://vgmdb.net/artist/%s'%artist).read()
+	data = data.decode('utf-8', 'ignore')
+	artist_info = vgmdb.artist.parse_artist_page(data)
+	return json.dumps(artist_info, sort_keys=True, indent=4, separators=(',',': '), ensure_ascii=False)
+print parse_artist(sys.argv[1])
