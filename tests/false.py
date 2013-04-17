9f3b3dbecb159154f818a94c3ec82fa32627a8a3
Walter Huf <hufman@gmail.com>
2013-04-16 22:10:09 -0500

    Remove dummy test modules

--- tests/false.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestFalse(unittest.TestCase):
-	def setUp(self):
-		pass
-
-	def test_false(self):
-		self.assertFalse(False)
-
--- tests/true.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestTrue(unittest.TestCase):
-	def setUp(self):
-		pass
-
-	def test_true(self):
-		self.assertTrue(True)
-
