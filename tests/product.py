72a51b4b79d13b34f9cc267fd04740cdf7279821
Walter Huf <hufman@gmail.com>
2013-04-17 21:40:55 -0500

    Actually verify that a product album listing generates the reprint tag

--- tests/product.py
+++ tests/product.py
@@ -39,3 +39,7 @@ class TestProducts(unittest.TestCase):
 		self.assertEqual(4, len(at['titles']))
 		self.assertEqual(u"Ar tonelico: Melody of Elemia", at['titles'][0]['name']['en'])
 		self.assertEqual(u"2010-12-26T03:20", at['meta']['added_date'])
+		two = [at['albums'][32], at['albums'][33]]
+		for thing in two:	# two albums released on the same date, hard to sort
+			if thing['link'] == '/album/20283':
+				self.assertEqual(True, thing['reprint'])
