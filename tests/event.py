4555bfa482a11a79240affa88a6ccf428fc36eab
Walter Huf <hufman@gmail.com>
2013-05-15 23:16:40 -0500

    Update the test cases to match the more consistent data

--- tests/event.py
+++ tests/event.py
@@ -18,9 +18,9 @@ class TestEvents(unittest.TestCase):
 		self.assertEqual(u'New Release', m3['releases'][0]['release_type'])
 		self.assertEqual(u'works', m3['releases'][0]['album_type'])
 		self.assertEqual(u'DVSP-0084', m3['releases'][1]['catalog'])
-		self.assertEqual(u'AD:TECHNO', m3['releases'][2]['title']['en'])
-		self.assertEqual(u'Clinochlore', m3['releases'][0]['publisher']['name']['en'])
-		self.assertEqual(u'Diverse System', m3['releases'][1]['publisher']['name']['en'])
+		self.assertEqual(u'AD:TECHNO', m3['releases'][2]['titles']['en'])
+		self.assertEqual(u'Clinochlore', m3['releases'][0]['publisher']['names']['en'])
+		self.assertEqual(u'Diverse System', m3['releases'][1]['publisher']['names']['en'])
 		self.assertEqual(u'/org/331', m3['releases'][1]['publisher']['link'])
 		self.assertEqual(u'2012-10-28', m3['releases'][5]['release_date'])
 
