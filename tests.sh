9d0551221e6961b22af9f2a93ce2e1af23d05231
Walter Huf <hufman@gmail.com>
2013-05-20 19:17:31 -0500

    Make the test suite return a failure if a test fails

--- tests.sh
+++ tests.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 dir=`dirname $0`
 oldpwd="$PWD"
+failed=0
 cd "$dir"
 for i in tests/*.py; do
 	testname=`basename $i .py`
 	echo "$testname" | grep "^_" > /dev/null && continue
 	echo "Running tests in $testname"
-	python -m unittest tests.$testname
+	python -m unittest tests.$testname || failed=1
 	echo
 done
 cd "$oldpwd"
+return $failed
