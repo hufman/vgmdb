1b881b7821acef61b24b8f5f2444ad4ae9dc66a8
Walter Huf <hufman@gmail.com>
2013-06-13 18:23:01 -0500

    Handles albums that are missing a category

--- vgmdb/output/rdf.py
+++ vgmdb/output/rdf.py
@@ -194,8 +194,10 @@ def generate_album(config, data):
 		g.add((subject, MO.other_release_of, reprinturi))
 		g.add((reprinturi, RDF.type, MO.Release))
 		g.add((reprinturi, MO.catalogue_number, Literal(reprint['catalog'])))
-	g.add((subject, SCHEMA.genre, Literal(data['category'])))
-	g.add((subject, MO.media_type, Literal(data['media_format'])))
+	if data.has_key('category'):
+		g.add((subject, SCHEMA.genre, Literal(data['category'])))
+	if data.has_key('media_format'):
+		g.add((subject, MO.media_type, Literal(data['media_format'])))
 	if data.has_key('rating'):
 		rating = BNode()
 		g.add((subject, SCHEMA.aggregateRating, rating))
