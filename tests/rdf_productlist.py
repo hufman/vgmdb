39ea86cae6ad0c25e9770ce66741dde51fef6a38
Walter Huf <hufman@gmail.com>
2013-06-19 22:45:07 -0500

    Unit tests for productlist output

---
+++ tests/rdf_productlist.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+import os
+import datetime
+import unittest
+
+from ._rdf import TestRDF
+from vgmdb import productlist
+from vgmdb.config import BASE_URL
+from urlparse import urljoin
+
+class TestArtistlistRDF(TestRDF):
+	data_parser = lambda self,x: productlist.parse_productlist_page(x)
+	outputter_type = 'productlist'
+	def setUp(self):
+		pass
+
+	def run_list_tests(self, graph):
+		test_count_results = {
+			"select ?product where { ?product rdf:type schema:CreativeWork . }" : 30
+		}
+		test_first_result = {
+			"select ?name where { <@baseproduct/856#subject> dcterms:title ?name . }" : u"Darius",
+			"select ?name where { <@baseproduct/856#subject> schema:name ?name . }" : u"Darius"
+		}
+		self.run_tests(graph, test_count_results, test_first_result)
+	def test_list_rdfa(self):
+		graph = self.load_rdfa_data('productlist.html')
+		self.run_list_tests(graph)
+	def test_list_rdf(self):
+		graph = self.load_rdf_data('productlist.html')
+		self.run_list_tests(graph)
+
