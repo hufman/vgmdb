8f2f78167930f62f362752cae8225802b1095bdd
Walter Huf <hufman@gmail.com>
2013-06-19 21:33:50 -0500

    Unit tests the artistlist parsing

---
+++ tests/artistlist.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+import os
+import unittest
+
+from vgmdb import artistlist
+
+base = os.path.dirname(__file__)
+
+class TestArtistList(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_list(self):
+		list_code = file(os.path.join(base, 'artistlist.html'), 'r').read()
+		list = artistlist.parse_artistlist_page(list_code)
+
+		self.assertEqual(99, len(list['artists']))
+		self.assertEqual(u"/artist/3535", list['artists'][0]['link'])
+		self.assertEqual(u"/artist/12702", list['artists'][3]['link'])
+		self.assertEqual(u"/artist/11699", list['artists'][96]['link'])
+		self.assertEqual(u"A BONE", list['artists'][0]['name'])
+		self.assertEqual(u"アービー", list['artists'][3]['name_real'])
