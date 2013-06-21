d0dce3f7f0e804d3d959b2c16ce9c9384459127d
Walter Huf <hufman@gmail.com>
2013-06-20 19:01:51 -0500

    Refactored url routing

--- vgmdb/main.py
+++ vgmdb/main.py
@@ -24,18 +24,24 @@ except:
 def hello():
 	return "Hello!"
 
-@route('/<type:re:(artist|album|product|event|org)>/<id:int>')
-def info(type,id):
-	prevdata = vgmdb.cache.get('vgmdb/%s/%s'%(type,id))
+def do_page(cache_key, page_type, url, link=None):
+	"""
+	@param cache_key where to save the parsed data
+	@param page_type what parser and output to use
+	@param url what url to download the data from
+	@param link what short url to set as the data['link']
+	"""
+	prevdata = vgmdb.cache.get(cache_key)
 	if not prevdata:
-		data = urllib.urlopen('http://vgmdb.net/%s/%s?perpage=99999'%(type,id)).read()
+		data = urllib.urlopen(url).read()
 		data = data.decode('utf-8', 'ignore')	# some pages have broken utf8
-		module = getattr(vgmdb, type)
-		parse_page = getattr(module, "parse_%s_page"%type)
+		module = getattr(vgmdb, page_type)
+		parse_page = getattr(module, "parse_%s_page"%page_type)
 		info = parse_page(data)
 		if info != None:
-			info['link'] = "/%s/%s"%(type,id)
-			vgmdb.cache.set('vgmdb/%s/%s'%(type,id), info)
+			if link:
+				info['link'] = link
+			vgmdb.cache.set(cache_key, info)
 	else:
 		info = prevdata
 	if info == None:
@@ -44,32 +50,27 @@ def info(type,id):
 	requested_format = request.query.format or ''
 	outputter = vgmdb.output.get_outputter(requested_format, request.headers.get('Accept'))
 	response.content_type = outputter.content_type
-	return outputter(type, info)
+	return outputter(page_type, info)
+
+@route('/<type:re:(artist|album|product|event|org)>/<id:int>')
+def info(type,id):
+	cache_key = 'vgmdb/%s/%s'%(type,id)
+	page_type = type
+	url = 'http://vgmdb.net/%s/%s?perpage=99999'%(type,id)
+	link = "/%s/%s"%(type,id)
+	return do_page(cache_key, page_type, url, link)
 
 @route('/<type:re:(albumlist|artistlist|productlist)>/<id:re:[#A-Z]>')
-def list(type,id):
+@route('/<type:re:(albumlist|artistlist|productlist)>/')
+@route('/<type:re:(albumlist|artistlist|productlist)>')
+def list(type,id='A'):
 	typeurls = {'albumlist': 'albums',
 	            'artistlist': 'artists',
 	            'productlist': 'product'}
-	prevdata = vgmdb.cache.get('vgmdb/%s/%s'%(type,id))
-	if not prevdata:
-		data = urllib.urlopen('http://vgmdb.net/db/%s.php?ltr=%s&field=title&perpage=9999'%(typeurls[type],id)).read()
-		data = data.decode('utf-8', 'ignore')	# some pages have broken utf8
-		module = getattr(vgmdb, "%s"%type)
-		parse_page = getattr(module, "parse_%s_page"%type)
-		info = parse_page(data)
-		if info != None:
-			info['link'] = "/%s/%s"%(type,id)
-			vgmdb.cache.set('vgmdb/%s/%s'%(type,id), info)
-	else:
-		info = prevdata
-	if info == None:
-		abort(404, "Item not found ")
-
-	requested_format = request.query.format or ''
-	outputter = vgmdb.output.get_outputter(requested_format, request.headers.get('Accept'))
-	response.content_type = outputter.content_type
-	return outputter(type, info)
+	cache_key = 'vgmdb/%s/%s'%(type,id)
+	page_type = type
+	url = 'http://vgmdb.net/db/%s.php?ltr=%s&field=title&perpage=9999'%(typeurls[type],id)
+	return do_page(cache_key, page_type, url)
 
 @route('/static/<name:path>')
 def static(name):
