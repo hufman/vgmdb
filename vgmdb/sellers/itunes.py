2498e968eb0da5abdc1b484a1ee97845bf4e6b06
Walter Huf <hufman@gmail.com>
2013-08-26 22:22:05 -0500

    Adds affiliate links to Amazon

--- vgmdb/sellers/itunes.py
+++ vgmdb/sellers/itunes.py
@@ -3,14 +3,40 @@ import urlparse
 import json
 import logging
 from ._utils import squash_str,find_best_match
+from .. import config
 
 SEARCH_API = 'https://itunes.apple.com/search?media=music&'
+AFFILIATE_ID = getattr(config, 'ITUNES_AFFILIATE_ID', None)
+TD_PROGRAM_ID = getattr(config, 'ITUNES_TD_PROGRAM_ID', None)
+TD_WEBSITE_ID = getattr(config, 'ITUNES_TD_WEBSITE_ID', None)
 
 class NullHandler(logging.Handler):
 	def emit(self, record):
 		pass
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
+
+def add_affiliate_id(link):
+	query = 3
+	TD_URL = "http://clk.tradedoubler.com/click?p=%s&a=%s&url=%s"
+	if AFFILIATE_ID:
+		parts = list(urlparse.urlsplit(link))
+		if parts[query] == '':
+			parts[query] += '?'
+		else:
+			parts[query] += '&'
+		parts[query] += 'at=%s'%(AFFILIATE_ID,)
+		link = urlparse.urlunsplit(parts)
+	if TD_PROGRAM_ID and TD_WEBSITE_ID:
+		parts = list(urlparse.urlsplit(link))
+		if parts[query] == '':
+			parts[query] += '?'
+		else:
+			parts[query] += '&'
+		parts[query] += 'partnerId=2003'
+		link = urlparse.urlunsplit(parts)
+		link = TD_URL % (TD_PROGRAM_ID, TD_WEBSITE_ID, urllib.quote(link))
+	return link
 
 def search_album(info):
 	search_url = SEARCH_API+'entity=album&term=%s'%(urllib.quote(squash_str(info['name'])),)
@@ -25,6 +51,7 @@ def search_album(info):
 		if found:
 			result['image'] = found['artworkUrl100']
 			result['found'] = found['collectionViewUrl']
+			result['found'] = add_affiliate_id(result['found'])
 	except:
 		import traceback
 		logger.warning(traceback.format_exc())
@@ -49,6 +76,7 @@ def search_artist(info):
 		if found:
 			result['surity'] = 'name'
 			result['found'] = found['artistLinkUrl']
+			result['found'] = add_affiliate_id(result['found'])
 	except:
 		import traceback
 		logger.warning(traceback.format_exc())
