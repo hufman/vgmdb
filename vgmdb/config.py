db5d117ab92c6a54fa072eb3400ef0aaa14bb880
Walter Huf <hufman@gmail.com>
2015-01-20 23:03:45 -0600

    Allows more of the AMQP settinsg to be specified
    Docker links don't carry things like passwords, so those are extra variables

--- vgmdb/config.py
+++ vgmdb/config.py
@@ -19,9 +19,16 @@ CELERY_CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 CELERY_PING = True
 
 # load cloud settings for defaults
-if 'AMQP_PORT_5672_TCP' in os.environ:
-	CELERY_BROKER = 'amqp://guest@%s//' % (
-	    os.environ['AMQP_PORT_5672_TCP_ADDR']
+if 'AMQP_PORT_5672_TCP' in os.environ or 'AMQP_HOST' in os.environ:
+	amqp_user = os.environ.get('AMQP_USER', 'guest')
+	amqp_pass = os.environ.get('AMQP_PASSWORD', 'guest')
+	amqp_ip = os.environ.get('AMQP_HOST', '127.0.0.1')
+	amqp_ip = os.environ.get('AMQP_PORT_5672_TCP_ADDR', amqp_ip)
+	amqp_port = os.environ.get('AMQP_PORT', '5672')
+	amqp_port = os.environ.get('AMQP_PORT_5672_TCP_PORT', amqp_port)
+	amqp_vhost = os.environ.get('AMQP_VHOST', '/')
+	CELERY_BROKER = 'amqp://%s:%s@%s:%s/%s' % (
+		amqp_user, amqp_pass, amqp_ip, amqp_port, amqp_vhost
 	)
 if 'MEMCACHED_PORT_11211_TCP' in os.environ:
 	CELERY_CACHE_BACKEND = 'memcached://%s:%s/' % (
