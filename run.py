c9ae0a96833c2945ac3a9e4c8a4cf3a79d2a8755
Walter Huf <hufman@gmail.com>
2015-07-17 17:02:30 -0500

    Adds gevent support

--- run.py
+++ run.py
@@ -3,9 +3,16 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-from bottle import run
+from vgmdb import config
+
+if config.USE_GEVENT:
+	from gevent import monkey; monkey.patch_all()
 
 from vgmdb import main
+from bottle import run
 
-run(host='0.0.0.0', port=9990)
+if config.USE_GEVENT:
+	run(host='0.0.0.0', port=9990, server='gevent')
+else:
+	run(host='0.0.0.0', port=9990)
 
