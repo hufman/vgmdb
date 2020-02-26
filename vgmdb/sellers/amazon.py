c9abb761b7e4f4645944f330b431a0ea7ca8eca3
Walter Huf <hufman@gmail.com>
2020-02-25 22:22:02 -0800

    Changes Amazon to use the new paapi5

--- vgmdb/sellers/amazon.py
+++ vgmdb/sellers/amazon.py
@@ -6,7 +6,7 @@ import time
 
 from .. import config
 from ._utils import squash_str,find_best_match, primary_name
-import amazonproduct
+import paapi5_python_sdk as paapi5
 
 class NullHandler(logging.Handler):
 	def emit(self, record):
@@ -21,38 +21,34 @@ else:
 
 if hasattr(config, 'AMAZON_ACCESS_KEY_ID') and config.AMAZON_ACCESS_KEY_ID and \
    hasattr(config, 'AMAZON_SECRET_ACCESS_KEY') and config.AMAZON_SECRET_ACCESS_KEY:
-	api_config = {
-		'access_key': config.AMAZON_ACCESS_KEY_ID,
-		'secret_key': config.AMAZON_SECRET_ACCESS_KEY,
-		'locale': 'us',
-		'associate_tag': getattr(config, 'AMAZON_ASSOCIATE_TAG', 'vgmdb')
-	}
-	#API = api.API(config.AMAZON_ACCESS_KEY_ID, config.AMAZON_SECRET_ACCESS_KEY, 'us', getattr(config, 'AMAZON_ASSOCIATE_TAG', 'vgmdb'))
-	API = amazonproduct.API(cfg=api_config)
+	API = paapi5.api.default_api.DefaultApi(
+		access_key = config.AMAZON_ACCESS_KEY_ID,
+		secret_key = config.AMAZON_SECRET_ACCESS_KEY,
+		host = "webservices.amazon.com",
+		region = "us-east-1"
+	)
 else:
 	API = None
 
-def parse_results(roots):
-	results = []
-	for root in roots:
-		nspace = root.nsmap.get(None, '')
-		xml_results = root.xpath('//aws:Item',
-		                         namespaces={'aws':nspace})
-		for xml_result in xml_results:
-			try:
-				url = xml_result.DetailPageURL.text
-				if '%3F' in url or '%ef' in url:
-					url = urllib.unquote(url)
-				result = {"DetailPageURL":url}
-
-				for child in xml_result.ItemAttributes.getchildren():
-					name = child.tag.split("}")[-1]
-					value = child.text
-					result[name] = value
-				results.append(result)
-			except:
-				pass
-	return results
+def run_search(keywords, artist=None):
+	search_items_resource = [paapi5.search_items_resource.SearchItemsResource.ITEMINFO_TITLE]
+	request = paapi5.search_items_request.SearchItemsRequest(
+		partner_tag = config.AMAZON_ASSOCIATE_TAG,
+		partner_type = paapi5.partner_type.PartnerType.ASSOCIATES,
+		keywords = keywords,
+		search_index = "Music",
+		item_count = 5,
+		resources = search_items_resource,
+		artist = artist
+	)
+	try:
+		response = API.search_items(request)
+	except paapi5.rest.ApiException:
+		time.sleep(2)
+		return None
+	if response.search_result is None:
+		return None
+	return response.search_result.items
 
 def empty_album(info):
 	search_url = SEARCH_PAGE + "&field-keywords=%s"%(urllib.quote(squash_str(info['name'])))
@@ -74,7 +70,7 @@ def search_album(info):
 			if found:
 				result['surity'] = 'album'
 		if found:
-			result['found'] = found['DetailPageURL']
+			result['found'] = found.detail_page_url
 	except:
 		import traceback
 		logger.warning(traceback.format_exc())
@@ -88,30 +84,23 @@ def search_artist_album_name(info):
 		return None
 	artist = primary_name(info['composers'][0]['names'])
 	title = info['name']
-	try:
-		results = parse_results(API.item_search('Music', limit=1, ResponseGroup='ItemAttributes', Artist=squash_str(artist), Title=squash_str(title)))
-	except amazonproduct.errors.TooManyRequests:
-		time.sleep(2)
-		return None
-	except amazonproduct.errors.NoExactMatchesFound:
+	results = run_search(keywords=squash_str(artist) + " " + squash_str(title),
+	                     artist=squash_str(artist))
+	if results is None:
 		return None
 	found = find_best_match(squash_str(title), results,
-	   threshold=0.5, key=lambda x:squash_str(x['Title']))
+	   threshold=0.5, key=lambda x:squash_str(x.item_info.title.display_value))
 	return found
 
 def search_album_name(info):
 	if not API:
 		return None
 	title = info['name']
-	try:
-		results = parse_results(API.item_search('Music', limit=1, ResponseGroup='ItemAttributes', Title=squash_str(title)))
-	except amazonproduct.errors.TooManyRequests:
-		time.sleep(2)
-		return None
-	except amazonproduct.errors.NoExactMatchesFound:
+	results = run_search(keywords=squash_str(title))
+	if results is None:
 		return None
 	found = find_best_match(squash_str(title), results,
-	   threshold=0.5, key=lambda x:squash_str(x['Title']))
+	   threshold=0.5, key=lambda x:squash_str(x.item_info.title.display_value))
 	return found
 
 def empty_artist(info):
@@ -127,6 +116,7 @@ def search_artist(info):
 		found = search_artist_name(info['name'])
 		if found:
 			result['surity'] = 'results'
+			# leave the search link in place
 			result['found'] = result['search']
 	except:
 		import traceback
@@ -137,18 +127,10 @@ def search_artist(info):
 def search_artist_name(name):
 	if not API:
 		return None
-	try:
-		results = parse_results(API.item_search('Music', limit=1, ResponseGroup='ItemAttributes', Artist=squash_str(name)))
-	except amazonproduct.errors.TooManyRequests:
-		time.sleep(2)
-	except amazonproduct.errors.NoExactMatchesFound:
-		return None
-	def get_artist(item):
-		if 'Artist' in item:
-			return item['Artist']
-		if 'Creator' in item:
-			return item['Creator']
-	found = find_best_match(squash_str(name), results,
-	   threshold=0.7, key=lambda x:squash_str(get_artist(x)))
-	return found
+	results = run_search(keywords=squash_str(name), artist=squash_str(name))
+	if results is not None:
+		return results[0]
+	results = run_search(keywords=squash_str(name))
+	if results is not None:
+		return results[0]
 
