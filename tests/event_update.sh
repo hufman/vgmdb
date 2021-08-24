55e76c84031a01edc99e18666242f6f213acf3ab
Walter Huf <hufman@gmail.com>
2021-08-23 23:52:45 -0700

    Updates event tests to the current html

---
+++ tests/event_update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://vgmdb.net/db/events.php -O eventlist.html
+wget https://vgmdb.net/event/152 -O event_m3.html
+wget https://vgmdb.net/event/28 -O event_cm54.html
--- tests/rdf_event.py
+++ tests/rdf_event.py
@@ -21,18 +21,18 @@ class TestEventRDF(TestRDF):
 			"select ?person where { <@base#subject> event:time <@base#release_event> . }" : 1,
 			"select ?date where { <@base#release_event> tl:start ?date . }" : 1,
 			"select ?date where { <@base#release_event> tl:end ?date . }" : 1,
-			"select ?album where { <@base#subject> mo:release ?album . }" : 39
+			"select ?album where { <@base#subject> mo:release ?album . }" : 30
 		}
 		test_first_result = {
-			"select ?date where { <@base#release_event> tl:start ?date . }" : datetime.date(2012,10,27),
-			"select ?date where { <@base#release_event> tl:end ?date . }" : datetime.date(2012,10,27),
-			"select ?date where { <@base#subject> schema:startDate ?date . }" : datetime.date(2012,10,27),
-			"select ?date where { <@base#subject> schema:endDate ?date . }" : datetime.date(2012,10,27),
+			"select ?date where { <@base#release_event> tl:start ?date . }" : datetime.date(2012,10,28),
+			"select ?date where { <@base#release_event> tl:end ?date . }" : datetime.date(2012,10,28),
+			"select ?date where { <@base#subject> schema:startDate ?date . }" : datetime.date(2012,10,28),
+			"select ?date where { <@base#subject> schema:endDate ?date . }" : datetime.date(2012,10,28),
 			"select ?catalog where { ?album mo:catalogue_number ?catalog . ?album schema:name ?title . ?album schema:name \"a Tale\"@en . }" : "N/A",
 			"select ?catalog where { ?album mo:catalogue_number ?catalog . ?album schema:name ?title . ?album schema:name \"AD:60\"@en . }" : "DVSP-0084",
 			"select ?release_date where { ?album dcterms:created ?release_date . ?album schema:name \"a Tale\"@en . }" : datetime.date(2012,10,28),
 			"select ?release_date where { ?album schema:datePublished ?release_date . ?album schema:name \"a Tale\"@en . }" : datetime.date(2012,10,28),
-			"select ?publisher where { ?album schema:name \"Hiten\"@en . ?album mo:publisher ?publisher . }" : "<@baseorg/1014#subject>",
+			"select ?publisher where { ?album schema:name \"AD:60\"@en . ?album mo:publisher ?publisher . }" : "<@baseorg/331#subject>",
 			"select ?name where { ?album mo:publisher ?publisher . ?publisher foaf:name ?name . ?album dcterms:title \"a Tale\"@en . filter(lang(?name)='en') }" : "Clinochlore"
 		}
 
@@ -55,10 +55,10 @@ class TestEventRDF(TestRDF):
 			"select ?date where { <@base#release_event> tl:end ?date . }" : 1,
 		}
 		test_first_result = {
-			"select ?date where { <@base#release_event> tl:start ?date . }" : datetime.date(1998,8,13),
-			"select ?date where { <@base#release_event> tl:end ?date . }" : datetime.date(1998,8,15),
-			"select ?date where { <@base#subject> schema:startDate ?date . }" : datetime.date(1998,8,13),
-			"select ?date where { <@base#subject> schema:endDate ?date . }" : datetime.date(1998,8,15),
+			"select ?date where { <@base#release_event> tl:start ?date . }" : datetime.date(1998,8,14),
+			"select ?date where { <@base#release_event> tl:end ?date . }" : datetime.date(1998,8,16),
+			"select ?date where { <@base#subject> schema:startDate ?date . }" : datetime.date(1998,8,14),
+			"select ?date where { <@base#subject> schema:endDate ?date . }" : datetime.date(1998,8,16),
 		}
 
 		self.run_tests(graph, test_count_results, test_first_result)
